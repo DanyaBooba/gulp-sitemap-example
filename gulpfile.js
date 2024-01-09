e7c492fc1116859b13464e32be195ca4aa41c565
Daniil Dybka <danil.dybko@gmail.com>
2024-01-09 16:10:03 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 const gulp = require("gulp");
 
-const feed = require("@zadkiel/gulp-feed");
-
-// const sitemap = require("gulp-sitemap");
+const rss = require("gulp-rss");
 
 // const fs = require("fs");
 // const turbo = require("turbo-rss");
@@ -48,36 +46,37 @@ const feed = require("@zadkiel/gulp-feed");
 
 // const xml = feed.xml();
 
-const posts = [gulp.src("src/**/*.html")];
-
-const options = {
-	transform: (post) => post,
-	render: {
-		"mysuperrss.xml": "rss2",
-	},
-};
-
-function get_sitemap() {
-	return gulp
-		.src("src/**/*.html", {
-			read: false,
-		})
-		.pipe(
-			sitemap({
-				siteUrl: "https://news.dybka.ru",
-				changefreq: "weekly",
-				priority: "0.5",
-			})
-		)
-		.pipe(gulp.dest("dist"));
-}
-
 //
 // Default Task
 //
 
 exports.default = gulp.series(function () {
-	feed(posts, options).pipe(gulp.dest("assets/"));
+	gulp
+		.src("./posts/*.md") // Read input files
+		.pipe(
+			rss({
+				// How we deal with contextual data (typically Front-Matter)
+				properties: {
+					data: "frontMatter", // name of property containing the data, typically extracted front-matter
+					// Proparty names mapping
+					title: "title", // post's title (means plugin will read `file.frontMatter.title`, mandatory)
+					link: "permalink", // post's URL (mandatory)
+					description: "description", // post's description (optional)
+					author: "author", // post's author (optional)
+					date: "date", // post's publication date (mandatory, default = now)
+					image: "image", // post's thumbnail (optional)
+				},
+
+				// Feed configuration
+				render: "atom-1.0", // Feed type (atom-1.0 or rss-2.0)
+				title: "My blog", // Feed title (mandatory)
+				description: "My very own blog", // Feed description (optional)
+				link: "http://my.bl.og", // Feed link (optional)
+				author: { name: "Nicolas Chambrier" }, // Blog's author (optional)
+				// etc…
+			})
+		)
+		.pipe(gulp.dest("./public/feed.xml")); // Write output
 });
 
 // var frontMatter = require("gulp-front-matter");
